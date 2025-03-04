13b5583ce6327be7d02c9f1dab50c48f9326d2b0
deepanshu <deepanshusoni613@gmail.com>
2025-03-04 23:43:59 +0530

    react-router

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,29 @@
-import NavTop from './NavTop.jsx'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NavTop from './NavTop'; // Import NavTop
+// import Home from './Home.jsx';
+// import Shop from './Shop.jsx';
+// import Contact from './Contact.jsx';
+// import About from './About.jsx';
+// import MyAccount from './MyAccount.jsx';
 
+
+// const reactRouter = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/shop", element: <Shop /> },
+//   { path: "/contact", element: <Contact /> },
+//   { path: "/about", element: <About /> },
+//   { path: "/myaccount", element: <MyAccount /> }
+ 
+// ]);
 
 function App() {
-
-
   return (
-    <NavTop/>
-  )
+    //<RouterProvider router={reactRouter}>//
+      <section className="flex flex-col min-h-screen">
+        <NavTop />
+      </section>
+    //</RouterProvider>//
+  );
 }
 
-export default App
+export default App;
--- ecommerce/src/NavTop.jsx
+++ ecommerce/src/NavTop.jsx
@@ -1,20 +1,42 @@
-
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
+import backimg from '../src/images/backimg.png';
+// import { a } from "react-router-dom";
 
 function NavTop() {
     return (
         <>
-            <nav className=" h-10 w-full border-2">
-                <ul className="text-black flex justify-end  gap-8 mr-20  py-1">
-                    <li>Home </li>
-                    <li>Shop</li>
-                    <li>My Account <i className="fa-solid fa-circle-user"></i></li>
-                    <li><i class="fa-solid fa-magnifying-glass"></i></li>
-                    <li>About</li>
+            <nav
+                id="nav1"
+                className="h-16 w-full rounded-2xl flex justify-between items-center py-3 my-1 bg-gray-800"
+            >
+                <h3 className="mx-5 text-2xl text-amber-50 font-bold">E-commerce Website</h3>
+                <ul className="flex justify-end gap-5 mr-20 text-amber-50">
+                    <li><a to="/">Home</a></li>
+                    <li><a to="/Shop">Shop</a></li>
+                    <li><a to="/Contact">Contact</a></li>
+                    <li><a to="/About">About</a></li>
+                    <li>
+                        <a to="/">
+                            <FaMagnifyingGlass className="h-8 w-5 cursor-pointer" />
+                        </a>
+                    </li>
+                    <li className="flex gap-1 items-center">
+                        <a to="/MyAccount" className="flex items-center gap-1">
+                            My Account <CgProfile className="h-8 w-5 cursor-pointer" />
+                        </a>
+                    </li>
                 </ul>
             </nav>
+            <div className="backimg">
+                <img
+                    src={backimg}
+                    alt="Background Image"
+                    className="w-full rounded-2xl opacity-75 h-[440px]"
+                />
+            </div>
         </>
     );
-
 }
-export default NavTop
+
+export default NavTop;
