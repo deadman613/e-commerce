3a3c72a0c547b016485eb86317024e8b566fb4b7
deepanshu <deepanshusoni613@gmail.com>
2025-03-07 23:54:27 +0530

    hamburger done

--- ecommerce/src/NavTop.jsx
+++ ecommerce/src/NavTop.jsx
@@ -9,19 +9,19 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 
 
-const NavTop= ()=> {
+const NavTop = () => {
 
-    const [Set,setVisible] = useState("false")
+    const [visible, setVisible] = useState(false)
     return (
         <>
             <nav
                 id="nav1"
-                className="h-16 w-full rounded flex justify-between items-center py-0 my-0 bg-white "
+                className="h-16 w-full sm:max-sm::w-[400px] gap-5  rounded flex justify-between items-center  bg-white "
             >
-                <h3 className="mx-5 text-2xl text-white font-bold flex ">E-commerce Website <FaShoppingCart className="py-1  mx-1.5 mt-1.5" /></h3>
+                <h3 className="mx-5 text-2xl  max-sm:text-[12px] text-white font-bold flex ">E-commerce Website <FaShoppingCart className="py-1  mx-1.5 mt-1.5" /></h3>
                 <div className="search  flex font-bold">
-                    <input type="text" className="text-white 
-                    h-10 w-auto rounded-2xl pt-2 pb-2 px border-2 border-white ml-1.5" />
+                    <input type="text" className="text-white xl:w-auto sm:max-sm:w-4 md:w[15vh]
+                    h-10 w-auto  rounded-2xl pt-2 pb-2 px border-2 border-white ml-1.5" />
                     <NavLink to="/">
                         <FaMagnifyingGlass className=" text-white h-8 w-5 pt-1.5 mx-2.5" />
                     </NavLink>
@@ -30,24 +30,24 @@ const NavTop= ()=> {
                 </div>
 
 
-                <ul className="flex justify-end gap-5 mr-20 cursor-pointer font-bold text-white">
+                <ul className=" hidden sm:flex justify-end gap-5 mr-20 cursor-pointer font-bold text-white">
 
-                    <li className='text-white' >
+                    <li className='text-white flex flex-col' >
                         <NavLink to="/" > Home <hr className=" w-4/4 border-none h-[1.5px] bg-white hidden" /></NavLink>
                     </li>
 
-                    <li className='text-white' >
+                    <li className='text-white flex flex-col' >
                         <NavLink to="/Shop" > Shop  <hr className=" w-4/4 border-none h-[1.5px] hidden bg-white" /> </NavLink>
                     </li>
 
-                    <li className='text-white' >
+                    <li className='text-white flex flex-col' >
                         <NavLink to="/Contact" > Contact  <hr className=" w-4/4 border-none h-[1.5px] bg-white hidden" /></NavLink>
                     </li>
-                    <li className='text-white' >
+                    <li className='text-white flex flex-col' >
                         <NavLink to="/About" > About  <hr className=" w-4/4 border-none h-[1.5px] bg-white hidden" /></NavLink>
                     </li>
 
-                    <li className=' relative cart text-white' >
+                    <li className=' relative cart text-white flex flex-col' >
                         <NavLink to="/Cart" ><IoBagOutline className=" pb-1.5 h-7 w-10 mx-1.5 font-bold " />  </NavLink>
 
 
@@ -66,23 +66,48 @@ const NavTop= ()=> {
                         </div>
 
                     </li>
-                    <li  className=" absolute mt-1.5 w-5 sm:hidden">
-                    <GiHamburgerMenu onClick={()=>setVisible("true")}  />
-                    </li>
+
                 </ul>
 
 
+                <div className="  text-white relative sm:hidden ">
+                    <GiHamburgerMenu onClick={() => setVisible(!visible)} className="text-white   mt-1.5 w-5  h-6 " />
+
+                </div>
             </nav>
+
+            <div className={`fixed top-0 right-0 h-full w-[250px] bg-white shadow-lg transform transition-transform duration-300 z-50 
+                ${visible ? "translate-x-0" : "translate-x-full"}`}>
+                <button 
+                    onClick={() => setVisible(false)} 
+                    className="absolute top-4 right-4 text-black text-xl font-bold"
+                >
+                    ✕
+                </button>
+                <ul className="flex flex-col mt-12 space-y-6 text-lg font-bold p-6">
+                    <li><NavLink to="/" className="outline-1 outline-black " onClick={() => setVisible(false)}>Home</NavLink></li>
+                    <li><NavLink to="/Shop" onClick={() => setVisible(false)}>Shop</NavLink></li>
+                    <li><NavLink to="/Contact" onClick={() => setVisible(false)}>Contact</NavLink></li>
+                    <li><NavLink to="/About" onClick={() => setVisible(false)}>About</NavLink></li>
+                    <li><NavLink to="/Cart" onClick={() => setVisible(false)}>Cart</NavLink></li>
+                    <li><NavLink to="/MyAccount" onClick={() => setVisible(false)}>My Account</NavLink></li>
+                </ul>
+            </div>
+
+
+
+
+
             <div className="backimg  relative flex">
                 <img
                     src={backimg}
                     alt="Background Image"
-                    className=" flex justify-center w-full rounded  h-[90vh]"
+                    className=" flex justify-center w-full rounded sm:max-sm:w-[400px]   h-[90vh]"
                 />
-                <h2 className="text-black absolute right-30  text-5xl   top-20"> 
-                <span className="text-red-500 ">Sale 20% off </span>
-                 <br /> On Everthing <br /> <span className="text-[15px] leading-2"> "Flash Sale! Grab Your Favorites  Before They’re Gone!"</span></h2>
-                <button  className="  absolute right-90 top-65 cursor-pointer bg-transparent outline-2 outline-offset-2 outline-red-500 rounded h-[30px] w-[100px]">Shop Now</button>
+                <h2 className="text-black absolute right-30  text-5xl top-20  sm:max-sm:right-20 sm:max-sm:top-90">
+                    <span className="text-red-500 ">Sale 20% off </span>
+                    <br /> On Everthing <br /> <span className="text-[15px] leading-2"> "Flash Sale! Grab Your Favorites  Before They’re Gone!"</span></h2>
+                <button className="  absolute  right-90 top-65 cursor-pointer bg-transparent outline-2 outline-offset-2 outline-red-500 rounded h-[30px] w-[100px]">Shop Now</button>
 
             </div>
         </>
