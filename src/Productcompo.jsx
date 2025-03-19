d9c87a68d114d58ad49be1ec2011c18dfe23d17e
deepanshu <deepanshusoni613@gmail.com>
2025-03-19 12:21:29 +0530

    product_ page_done

--- src/Productcompo.jsx
+++ src/Productcompo.jsx
@@ -19,11 +19,6 @@ const Productcompo= ({name,id,price,image})=>
             <p className="text-sm pr-1.5">{currency}{price}</p>
         
 
-            <div className="sm:overflow-hidden  scale-90 hover:scale-95 border mr-10 sm:ml-0 rounded-2xl border-[#414141]">
-                <img className="rounded-2xl h-[200px] w-[200px] sm:h-60 sm:w-70" src={image[0]} alt="" />
-             </div>
-            <p className="pt-3 pb-1 text-sm sm:ml-4  ">{name}</p>
-            <p className="text-sm pr-1.5 sm:ml-4 ">{currency}{price}</p>
 
         </NavLink>
         
